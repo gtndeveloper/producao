d252a2650e41747732df9d763d1c4fb1b75f605c
gtndeveloper <gtnxdoj2@gmail.com>
2022-05-12 22:01:25 -0300

    commit

--- client.lua
+++ client.lua
@@ -1,3 +1,15 @@
 local Tunnel = module("vrp","lib/Tunnel")
 local Proxy = module("vrp","lib/Proxy")
 vRP = Proxy.getInterface("vRP")
+
+local Open = false
+function ToggleActionMenu()
+	open = not open
+	if open then
+		SetNuiFocus(true,true)
+		SendNUIMessage({ Open = true })
+	else
+		SetNuiFocus(false)
+		SendNUIMessage({ Open = false })
+	end
+end
